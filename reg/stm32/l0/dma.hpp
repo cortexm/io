924e2d3042dbee3c68a1dd1b99eb9094432fab78
Pavel Revak <pavel.revak@gmail.com>
2018-02-05 21:56:07 +0100

    added ADC for L0

---
+++ reg/stm32/l0/dma.hpp
@@ -0,0 +1,29 @@
+/**
+* Peripheral Definition File
+*
+* DMA - Direct memory access controller
+*
+* MCUs containing this peripheral:
+*  - STM32F0xx
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+#include "io/reg/stm32/_common/dma_v1.hpp"
+
+namespace io {
+
+namespace base {
+
+static const size_t DMA1 = 0x40026000;
+static const size_t DMA2 = 0x40026400;
+
+}
+
+static Dma &DMA1 = *reinterpret_cast<Dma *>(base::DMA1);
+static Dma &DMA2 = *reinterpret_cast<Dma *>(base::DMA2);
+
+}
