6d0cf7a9a324a5e3165a8406ecb58b09dbd0121e
Pavel Revak <pavel.revak@gmail.com>
2018-02-13 22:43:38 +0100

    support RTC for F3

---
+++ reg/stm32/f3/rtc.hpp
@@ -0,0 +1,27 @@
+/**
+* Peripheral Definition File
+*
+* RTC - Real-time clock
+*
+* MCUs containing this peripheral:
+*  - STM32F0xx
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+#include "io/reg/stm32/_common/rtc_v2.hpp"
+
+namespace io {
+
+namespace base {
+
+static const size_t RTC = 0x40002800;
+
+}
+
+static Rtc &RTC = *reinterpret_cast<Rtc *>(base::RTC);
+
+}
