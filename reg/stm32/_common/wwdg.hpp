7f341788eb160b0c0520a5dee4e75ff0f80fd241
Pavel Revak <pavel.revak@gmail.com>
2018-02-21 12:12:24 +0100

    added WWDG peripheral for Fx and Lx

---
+++ reg/stm32/_common/wwdg.hpp
@@ -0,0 +1,82 @@
+/**
+* Peripheral Definition File
+*
+* WWDG - Window watchdog
+*
+* MCUs containing this peripheral:
+*  - STM32F0xx
+*  - STM32F1xx
+*  - STM32F2xx
+*  - STM32F3xx
+*  - STM32F4xx
+*  - STM32F7xx
+*  - STM32L0xx
+*  - STM32L1xx
+*  - STM32L4xx
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+namespace io {
+
+struct Wwdg {
+    /** Control register
+     */
+    struct Cr {
+        Cr() {}
+        Cr(uint32_t raw) { r = raw; }
+        union {
+            uint32_t r;
+            struct {
+                uint32_t T : 7;  // 7-bit counter (MSB to LSB)
+                uint32_t WDGA : 1;  // Activation bit
+                uint32_t : 24;
+            } b;
+        };
+    };
+
+    /** Configuration register
+     */
+    struct Cfr {
+        Cfr() {}
+        Cfr(uint32_t raw) { r = raw; }
+        union {
+            uint32_t r;
+            struct {
+                uint32_t T : 7;  // 7-bit window value
+                uint32_t WDGTB : 2;  // Timer base
+                uint32_t EWI : 1;  // Early wakeup interrupt
+                uint32_t : 22;
+            } b;
+        };
+        struct Wdgtb {
+            static const uint32_t DIV_1 = 0;
+            static const uint32_t DIV_2 = 1;
+            static const uint32_t DIV_4 = 2;
+            static const uint32_t DIV_8 = 3;
+        };
+    };
+
+    /** Status register
+     */
+    struct Sr {
+        Sr() {}
+        Sr(uint32_t raw) { r = raw; }
+        union {
+            uint32_t r;
+            struct {
+                uint32_t EWIF : 1;  // Early wakeup interrupt flag
+                uint32_t : 31;
+            } b;
+        };
+    };
+
+    volatile Cr CR;  // Control register
+    volatile Cfr CFR;  // Configuration register
+    volatile Sr SR;  // Status register
+};
+
+}
---
+++ reg/stm32/f1/wwdg.hpp
@@ -0,0 +1,27 @@
+/**
+* Peripheral Definition File
+*
+* WWDG - Independent watchdog
+*
+* MCUs containing this peripheral:
+*  - STM32F1xx
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+#include "io/reg/stm32/_common/wwdg.hpp"
+
+namespace io {
+
+namespace base {
+
+    static constexpr size_t WWDG = 0x40002c00;
+
+}
+
+static Wwdg &WWDG = *reinterpret_cast<Wwdg *>(base::WWDG);
+
+}
