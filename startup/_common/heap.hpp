3f2d63f955371d30987a290ee66240735d22471e
Pavel Revak <pavel.revak@gmail.com>
2018-08-04 00:39:33 +0200

    removed register keyword, because it is not compatible with C++11

--- startup/_common/heap.hpp
+++ startup/_common/heap.hpp
@@ -11,7 +11,7 @@ extern unsigned __heap_start;
  */
 inline void fill_heap(unsigned fill=0x55555555) {
     unsigned *dst = &__heap_start;
-    register unsigned *msp_reg;
+    unsigned *msp_reg;
     __asm__("mrs %0, msp\n" : "=r" (msp_reg) );
     while (dst < msp_reg) {
         *dst++ = fill;
