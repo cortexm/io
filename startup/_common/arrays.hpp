39a6e2bc9c2eefef89a1aed00fd1e111eb1d537b
Pavel Revak <pavel.revak@gmail.com>
2018-09-08 00:29:39 +0200

    better init arrays

--- startup/_common/arrays.hpp
+++ startup/_common/arrays.hpp
@@ -4,43 +4,46 @@
 
 #pragma once
 
-typedef void (*array_func_t)();
+typedef void (*array_func_t[])();
 
 // pre-constructors array
-extern void (*__preinit_array_start[])() __attribute__((weak));
-extern void (*__preinit_array_end[])() __attribute__((weak));
+extern array_func_t __preinit_array_start;
+extern array_func_t __preinit_array_end;
 
 /** call pre-constructors for static objects
  */
 inline void call_preinit_array() {
-    int count = __preinit_array_end - __preinit_array_start;
-    for (int i = 0; i < count; i++) {
-        __preinit_array_start[i]();
+    auto array = __preinit_array_start;
+    while (array < __preinit_array_end) {
+        (*array)();
+        array++;
     }
 }
 
 // constructors array
-extern void (*__init_array_start[])() __attribute__((weak));
-extern void (*__init_array_end[])() __attribute__((weak));
+extern array_func_t __init_array_start;
+extern array_func_t __init_array_end;
 
 /** call constructors for static objects
  */
 inline void call_init_array() {
-    int count = __init_array_end - __init_array_start;
-    for (int i = 0; i < count; i++) {
-        __init_array_start[i]();
+    auto array = __init_array_start;
+    while (array < __init_array_end) {
+        (*array)();
+        array++;
     }
 }
 
 // destructors array
-extern void (*__fini_array_start[])() __attribute__((weak));
-extern void (*__fini_array_end[])() __attribute__((weak));
+extern array_func_t __fini_array_start;
+extern array_func_t __fini_array_end;
 
 /** call destructors for static objects
  */
 inline void call_fini_array() {
-    int count = __fini_array_end - __fini_array_start;
-    for (int i = 0; i < count; i++) {
-        __fini_array_start[i]();
+    auto array = __fini_array_start;
+    while (array < __fini_array_end) {
+        (*array)();
+        array++;
     }
 }
