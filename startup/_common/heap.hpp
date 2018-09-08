7c3d0838a56636e714e07041b69497c57b25b1b2
Pavel Revak <pavel.revak@gmail.com>
2018-09-08 23:23:16 +0200

    changed patter of free memory to 'FREE'

--- startup/_common/heap.hpp
+++ startup/_common/heap.hpp
@@ -7,9 +7,9 @@
 // heap start
 extern unsigned __heap_start;
 
-/** Clear/fill content of HEAP memory
+/** Clear/fill with pattern content of HEAP memory
  */
-inline void fill_heap(unsigned fill=0x55555555) {
+inline void fill_heap(unsigned fill=0x45455246) {
     unsigned *dst = &__heap_start;
     unsigned *msp_reg;
     __asm__("mrs %0, msp\n" : "=r" (msp_reg) );
